7e9793439372c843c6dae560930a96fceb0df639
mayu <white_hot_chocolate@i.softbank.jp>
2012-03-24 02:17:23 +0900

    refs #10 $ rvmsudo rails new room -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Room::Application.config.secret_token = 'e78cc254f8a7630d0aa8eea33f643b55622ce12849f6a57920be5249dbd829e6ae0c4570bc27bad76fd294374746d305cfc05098b1a68583c63bfcbd187ff971'
