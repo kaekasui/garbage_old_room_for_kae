55b76b8da0b307e5f6ba1038d105a8df36dd0f4e
mayu <white_hot_chocolate@i.softbank.jp>
2012-03-25 02:30:34 +0900

    fixes #3 added side menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :side_menu
+
+
+  private
+
+  def side_menu
+    @menus = Menu.find_all_by_disable(true)
+  end
 end
