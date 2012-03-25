4e6d3e0f173c883484d8f360ac2cd594c555431c
mayu <white_hot_chocolate@i.softbank.jp>
2012-03-25 10:59:43 +0900

    fixes #6 added contents1 and continue of blogs

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,8 @@
 module BlogsHelper
+
+  # ブログ記事のサニタイズを行う
+  def sanitize_blog(id, contents)
+    @target = contents ? Blog.find(id).contents1 : Blog.find(id).contents2
+    sanitize(@target, :tags => %w(img a), :attributes => %w(src href))
+  end
 end
