4f86200c81cfadf05bd9de045f2919e9bc85557f
mayu <white_hot_chocolate@i.softbank.jp>
2012-03-30 22:41:35 +0900

    refs #45 created function to create blog comment

--- app/controllers/blog_comments_controller.rb
+++ app/controllers/blog_comments_controller.rb
@@ -1,55 +1,29 @@
 class BlogCommentsController < ApplicationController
-  # GET /blog_comments
-  # GET /blog_comments.json
-  def index
-    @blog_comments = BlogComment.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @blog_comments }
-    end
-  end
 
-  # GET /blog_comments/1
-  # GET /blog_comments/1.json
-  def show
-    @blog_comment = BlogComment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @blog_comment }
-    end
-  end
-
-  # GET /blog_comments/new
-  # GET /blog_comments/new.json
-  def new
-    @blog_comment = BlogComment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @blog_comment }
-    end
-  end
-
-  # GET /blog_comments/1/edit
-  def edit
-    @blog_comment = BlogComment.find(params[:id])
+  def confirm
+    @blog_comment = BlogComment.new(params[:blog_comment])
   end
 
   # POST /blog_comments
   # POST /blog_comments.json
   def create
     @blog_comment = BlogComment.new(params[:blog_comment])
+    @blog_comment.update_attributes(blog_id: params[:blog_id], ip: request.remote_ip)
 
     respond_to do |format|
       if @blog_comment.save
-        format.html { redirect_to @blog_comment, notice: 'Blog comment was successfully created.' }
+        format.html { redirect_to blog_path(@blog_comment.blog_id), notice: 'Blog comment was successfully created.' }
         format.json { render json: @blog_comment, status: :created, location: @blog_comment }
       else
         format.html { render action: "new" }
         format.json { render json: @blog_comment.errors, status: :unprocessable_entity }
-      end
-    end
+      end 
+    end 
+  end
+
+  # GET /blog_comments/1/edit
+  def edit
+    @blog_comment = BlogComment.find(params[:id])
   end
 
   # PUT /blog_comments/1
