f928df6b3b4e3e47c5d193a98a6d968f687a1776
mayu <white_hot_chocolate@i.softbank.jp>
2012-04-02 01:26:51 +0900

    fixes #76 enhancement of error

--- app/models/admin/version.rb
+++ app/models/admin/version.rb
@@ -2,6 +2,6 @@ class Admin::Version < ActiveRecord::Base
   set_table_name :versions
 
   def self.current_version
-    "room-" + self.find_by_current(true).name
+    "room-" + self.find_by_current(true).name if self.find_by_current(true)
   end
 end
